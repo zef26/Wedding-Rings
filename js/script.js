5f86b7457fb2703351aa3b88902f21d9d002995c
zef26 <salavat2602@mail.ru>
2023-04-26 14:46:35 +0500

    4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 let doc = document
 
+const anchors = doc.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+anchor.addEventListener('click', function(event){
+event.preventDefault()
+const  blockID = anchor.getAttribute('href')
+document.querySelector('' + blockID).scrollIntoView({
+    behavior:'smooth',
+    block: 'center'
+})
+})
+}
 
 
 let swiper = new Swiper(".swiper", {
