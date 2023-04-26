600cac946bfddf76183a78f6e11697d8058567ae
zef26 <salavat2602@mail.ru>
2023-04-26 14:16:31 +0500

    2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 let doc = document
+
+
+
+let swiper = new Swiper(".swiper", {
+    slidesPerView: 5,
+    spaceBetween: 0,
+    
+    freeMode: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      
+      
+    },
+  });
